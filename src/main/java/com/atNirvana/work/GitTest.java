2c670f41ffa5a947fbffdafc770ed28708f0136a
Nirvana-Zml <15014166337@163.com>
2025-03-30 17:05:18 +0800

    pull commit

--- src/main/java/com/atNirvana/work/GitTest.java
+++ src/main/java/com/atNirvana/work/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Master Test");
         System.out.println("hot-fix test");
         System.out.println("push test01");
+        System.out.println("pull test");
     }
 }

2c1a73e766fe8f6da44f02cd40bfbfe4d8551f79
Nirvana <15014166337@163.com>
2025-03-30 16:05:52 +0800

    Second commit

--- src/main/java/com/atNirvana/work/GitTest.java
+++ src/main/java/com/atNirvana/work/GitTest.java
@@ -3,5 +3,6 @@ package com.atNirvana.work;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("GitTest01");
+        System.out.println("GitTest02");
     }
 }
